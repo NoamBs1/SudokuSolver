ed91d0cfccd7485e9a336f34cd2164e07e19db7e
Noam Bs <noambenshoshan104@gmail.com>
2024-02-06 18:42:43 +0200

    cell class, represent a cell in the board

---
+++ SudokuProject/Cell.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuProject
+{
+    internal class Cell
+    {
+        public char Clue { get; set; }
+        public ISet<char> Candidates { get; set; }
+
+        public Cell() 
+        {
+            this.Clue = '0';
+            this.Candidates = new HashSet<char>(Constants.Values);
+        }
+
+        public Cell(char clue)
+        {
+            this.Clue = clue;
+            this.Candidates = new HashSet<char>(Constants.Values);
+            Candidates.Remove(clue);
+        }
+    }
+}
