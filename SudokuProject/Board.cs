39dc22f53e9b9080f6ea2feadd28a8774fc416b4
Noam Bs <noambenshoshan104@gmail.com>
2024-02-06 18:41:02 +0200

    the board interface, and board class that implements it.

---
+++ SudokuProject/Board.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuProject
+{
+    internal class Board : IBoard
+    {
+        public int Size { get; set; }
+        public Cell[,] Matrix { get; set; }
+        private readonly IBoardPrinter printer;
+
+        public Board()
+        {
+            
+        }
+        public Board(int size, IBoardPrinter printer)
+        {
+            this.Size = size;
+            this.Matrix = new Cell[size, size];
+            this.printer = printer;
+        }
+
+        public void AddToBoard(int row, int col, char val)
+        {
+            if(row < 0 || col < 0 || row > this.Size || col > this.Size)
+                throw new ArgumentOutOfRangeException("row or column not valid");
+            if(val == '0')
+                this.Matrix[row, col] = new Cell();
+            else
+                this.Matrix[row, col] = new Cell(val);
+        }
+
+        public void PrintBoard()
+        {
+            printer.Print(this);
+        }
+    }
+}
---
+++ SudokuProject/IBoard.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuProject
+{
+    internal interface IBoard
+    {
+        int Size { get; set; }
+        void AddToBoard(int row, int col, char val);
+    }
+}
