7039b1dc349779d77e7a5cd377fde0bc659a4e6d
Noam Bs <noambenshoshan104@gmail.com>
2024-02-07 12:56:52 +0200

    interface and classes that implememts it, of ways to get the input board

---
+++ SudokuProject/FileInputHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuProject
+{
+    internal class FileInputHandler : IInputHandler
+    {
+        private readonly string _fileName;
+        public FileInputHandler(string fileName) 
+        { 
+            _fileName = fileName;
+        }
+
+        public string GetInput()
+        {
+            return File.ReadAllText(_fileName);
+        }
+    }
+}
