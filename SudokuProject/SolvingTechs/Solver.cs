8def8b4bb7e2c71566a9f15ec76b79ac63be998f
Noam Bs <noambenshoshan104@gmail.com>
2024-02-14 20:55:38 +0200

    The solver was modified, and i added the naked single idea during the adding to board logic.

--- SudokuProject/SolvingTechs/Solver.cs
+++ SudokuProject/SolvingTechs/Solver.cs
@@ -9,21 +9,29 @@ using System.Threading.Tasks;
 
 namespace SudokuProject
 {
+    /// <summary>
+    /// the class that solving the board
+    /// </summary>
     internal partial class Solver
     {
         public IBoard Board { get; set; }
+
+        /// <summary>
+        /// Initialized a solver with the required board
+        /// </summary>
+        /// <param name="board">the board you want to be solved</param>
         public Solver(IBoard board)
         {
             Board = board;
         }
+        /// <summary>
+        /// solving the board
+        /// </summary>
+        /// <param name="board">the board you want to be solved</param>
+        /// <returns>the solved board</returns>
 
         public IBoard Solve(IBoard board)
         {
-            bool flag = true;
-            while (flag)
-            {
-                flag = NakedSingle(board);
-            }
             (int row, int col) minCell = FindCellMinCandidates(board);
             if (minCell == (-1, -1))
             {
@@ -42,10 +50,16 @@ namespace SudokuProject
             return null;
         }
 
+
+        /// <summary>
+        /// finds the cell with minimal candidates
+        /// </summary>
+        /// <param name="board">the board you want to be solved</param>
+        /// <returns>the indexes of the cell with the min candidates</returns>
         public (int, int) FindCellMinCandidates(IBoard board)
         {
             (int, int) minCell = (-1, -1);
-            int minCandidates = board.Size;
+            int minCandidates = board.Size + 1;
             for (int i = 0; i < board.Size; i++)
             {
                 for (int j = 0; j < board.Size; j++)
@@ -54,18 +68,30 @@ namespace SudokuProject
                     {
                         minCell = (i, j);
                         minCandidates = board.Matrix[i, j].Candidates.Count;
+                        if (minCandidates == 1)
+                            return minCell;
                     }
                 }
             }
             return minCell;
         }
 
-        public bool IsBoardValidAndComplete(IBoard b)
+
+        /// <summary>
+        /// checks if all the board is valid and completed
+        /// </summary>
+        /// <param name="b">the board you want to check</param>
+        /// <returns>true if it is valid and completed, otherwise false</returns>
+        public static bool IsBoardValidAndComplete(IBoard b)
         {
             return IsRowsValidAndComplete(b) && IsColumnsValidAndComplete(b) && IsBlocksValidAndComplete(b);
         }
-
-        public bool IsRowsValidAndComplete(IBoard b)
+        /// <summary>
+        /// checks if all the rows are valid and completed
+        /// </summary>
+        /// <param name="b">the board you want to check</param>
+        /// <returns>true if it is valid and completed, otherwise false</returns>
+        public static bool IsRowsValidAndComplete(IBoard b)
         {
             for (int i = 0; i < b.Size; i++)
             {
@@ -74,8 +100,12 @@ namespace SudokuProject
             }
             return true;
         }
-
-        public bool IsColumnsValidAndComplete(IBoard b)
+        /// <summary>
+        /// checks if all the cols are valid and completed
+        /// </summary>
+        /// <param name="b">the board you want to check</param>
+        /// <returns>true if it is valid and completed, otherwise false</returns>
+        public static bool IsColumnsValidAndComplete(IBoard b)
         {
             for (int i = 0; i < b.Size; i++)
             {
@@ -84,8 +114,12 @@ namespace SudokuProject
             }
             return true;
         }
-
-        public bool IsBlocksValidAndComplete(IBoard b)
+        /// <summary>
+        /// checks if all the boxes are valid and completed
+        /// </summary>
+        /// <param name="b">the board you want to check</param>
+        /// <returns>true if it is valid and completed, otherwise false</returns>
+        public static bool IsBlocksValidAndComplete(IBoard b)
         {
             int sizeOfBlock = (int)Math.Sqrt(b.Size);
             for (int startRow = 0; startRow < b.Size; startRow += sizeOfBlock)
@@ -98,81 +132,73 @@ namespace SudokuProject
             }
             return true;
         }
-
-        public bool IsSingleRowValidAndComplete(IBoard b, int row)
+        /// <summary>
+        /// checks if one row is valid and completed
+        /// </summary>
+        /// <param name="b">the board you want to check</param>
+        /// <param name="row">the row you want to check</param>
+        /// <returns>true if it is valid and completed, otherwise false</returns>
+        public static bool IsSingleRowValidAndComplete(IBoard b, int row)
         {
-            HashSet<char> allChars = new HashSet<char>();
+            int[] allChars = new int[b.Size + 1];
             for (int i = 0; i < b.Size; i++)
             {
                 char clue = b.Matrix[row, i].Clue;
-                if (clue == '0' || !allChars.Add(clue))
+                if (clue == '0')
                     return false;
+                allChars[clue - '0']++;
             }
+            for (int i = 1; i < b.Size + 1; i++)
+                if (allChars[i] > 1)
+                    return false;
             return true;
         }
-
-        public bool IsSingleColoumnValidAndComplete(IBoard b, int column)
+        /// <summary>
+        /// checks if one column is valid and completed
+        /// </summary>
+        /// <param name="b">the board you want to check</param>
+        /// <param name="column">the column you want to check</param>
+        /// <returns>true if it is valid and completed, otherwise false</returns>
+        public static bool IsSingleColoumnValidAndComplete(IBoard b, int column)
         {
-            HashSet<char> allChars = new HashSet<char>();
+            int[] allChars = new int[b.Size + 1];
             for (int i = 0; i < b.Size; i++)
             {
                 char clue = b.Matrix[i, column].Clue;
-                if (clue == '0' || !allChars.Add(clue))
+                if (clue == '0')
                     return false;
+                allChars[clue - '0']++;
             }
+            for (int i = 1; i < b.Size + 1; i++)
+                if (allChars[i] > 1)
+                    return false;
             return true;
         }
-
-        public bool IsSingleBoxValidAndComplete(IBoard b, int startRow, int startColumn)
+        /// <summary>
+        /// checks if one box is valid and completed
+        /// </summary>
+        /// <param name="b">the board you want to check</param>
+        /// <param name="startRow">the start row of the box</param>
+        /// <param name="startColumn">the start column of the box</param>
+        /// <returns>true if it is valid and completed, otherwise false</returns>
+        public static bool IsSingleBoxValidAndComplete(IBoard b, int startRow, int startColumn)
         {
-            HashSet<char> allChars = new HashSet<char>();
+            int[] allChars = new int[b.Size + 1];
             int sizeOfBlock = (int)Math.Sqrt(b.Size);
             for (int i = startRow; i < startRow + sizeOfBlock; i++)
             {
                 for (int j = startColumn; j < startColumn + sizeOfBlock; j++)
                 {
                     char clue = b.Matrix[i, j].Clue;
-                    if (clue == '0' || !allChars.Add(clue))
+                    if (clue == '0')
                         return false;
+                    allChars[clue - '0']++;
                 }
             }
+            for (int i = 1; i < b.Size + 1; i++)
+                if (allChars[i] > 1)
+                    return false;
             return true;
         }
-
-
-        /*
-        public bool NakedPair()
-        {
-            
-        }
-        
-
-        public void DeleteCandidateFromRow(int row, int col, char candidate)
-        {
-            for(int i=0; i< Board.Size;i++)
-            {
-                if(i!=col)
-                    Board.Matrix[row, i].RemoveCandidate(candidate);
-
-            }
-        }
-        public (int, int)? FindPairInRow(int row)
-        {
-            for (int i = 0;i<Board.Size;i++)
-            {
-                if (Board.Matrix[row,i].Candidates.Count == 2)
-                {
-                    for(int j=i+1;j<Board.Size;j++)
-                    {
-                        if (Board.Matrix[row, j].Candidates.Count == 2 &&
-                            Board.Matrix[row, j].Candidates.SetEquals(Board.Matrix[row, i].Candidates))
-                            return (i, j);
-                    }
-                }
-            }
-            return null;
-        }
-        
-        */
     }
 }
